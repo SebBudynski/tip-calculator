83c381c805480467f63b6248543cb47541b59894
Sebastian Budyński <Sebbudynski@gmail.com>
2024-02-11 17:40:43 +0100

    Build some of input and reset functionalities

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 "use strict";
 
+const billInput = document.querySelector("#bill-amount");
 const customInput = document.querySelector("#number-input");
 const resetBtn = document.querySelector("#reset");
+const peopleInput = document.querySelector("#number-of-people");
+
+const sliceInput = (input) => {
+  if (input.value.length > 6) {
+    input.value = input.value.slice(0, 6);
+  }
+};
+
+const displayError = (input) => {
+  if (input.value === "" || input.value === "0") {
+    input.classList.add("error");
+  }
+};
 
 customInput.addEventListener("focus", () => {
   customInput.placeholder = "";
@@ -12,6 +26,25 @@ customInput.addEventListener("blur", () => {
 });
 
 resetBtn.addEventListener("click", () => {
+  peopleInput.classList.remove("error");
+  billInput.value = "";
   customInput.value = "";
   customInput.placeholder = "Custom";
+  peopleInput.value = "";
+});
+
+billInput.addEventListener("input", () => {
+  sliceInput(billInput);
+});
+
+peopleInput.addEventListener("input", () => {
+  sliceInput(peopleInput);
+});
+
+peopleInput.addEventListener("blur", () => {
+  displayError(peopleInput);
+});
+
+customInput.addEventListener("input", () => {
+  sliceInput(customInput);
 });
