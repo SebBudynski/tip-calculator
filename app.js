b0689f3233b6b838c2a0a18814515c9c9d952f55
Sebastian Budyński <Sebbudynski@gmail.com>
2024-02-12 17:40:12 +0100

    Add part of funcionalities to JS

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 "use strict";
 
 const billInput = document.querySelector("#bill-amount");
-const customInput = document.querySelector("#number-input");
+const customInput = document.querySelector("#custom-input");
 const resetBtn = document.querySelector("#reset");
 const peopleInput = document.querySelector("#number-of-people");
+const amount = document.querySelector(".amount");
+const tipBtns = document.querySelectorAll("tip-btn");
+let tipValue = 0;
 
 const sliceInput = (input) => {
   if (input.value.length > 6) {
@@ -14,6 +17,26 @@ const sliceInput = (input) => {
 const displayError = (input) => {
   if (input.value === "" || input.value === "0") {
     input.classList.add("error");
+  } else {
+    input.classList.remove("error");
+  }
+};
+
+tipBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    tipValue = parseFloat(this.value);
+    calcTip();
+  });
+});
+
+const calcTip = () => {
+  let billValue = parseFloat(billInput.value);
+  let peopleValue = parseInt(peopleInput.value);
+  let tipAmount = (billValue / peopleValue) * tipValue;
+  if (Number.isInteger(tipAmount)) {
+    amount.textContent = tipAmount.toFixed(0);
+  } else {
+    amount.textContent = tipAmount.toFixed(2);
   }
 };
 
@@ -27,6 +50,7 @@ customInput.addEventListener("blur", () => {
 
 resetBtn.addEventListener("click", () => {
   peopleInput.classList.remove("error");
+  billInput.classList.remove("error");
   billInput.value = "";
   customInput.value = "";
   customInput.placeholder = "Custom";
@@ -35,10 +59,17 @@ resetBtn.addEventListener("click", () => {
 
 billInput.addEventListener("input", () => {
   sliceInput(billInput);
+  calcTip();
+  console.log(calcTip());
+});
+
+billInput.addEventListener("blur", () => {
+  displayError(billInput);
 });
 
 peopleInput.addEventListener("input", () => {
   sliceInput(peopleInput);
+  calcTip();
 });
 
 peopleInput.addEventListener("blur", () => {
