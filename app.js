6c43c0b0f4fc7299634bd5fc1f178e9b7472a128
Sebastian Budyński <Sebbudynski@gmail.com>
2024-02-13 15:45:48 +0100

    Add bluredfunction to reset button

--- app.js
+++ app.js
@@ -2,16 +2,37 @@
 
 const billInput = document.querySelector("#bill-amount");
 const customInput = document.querySelector("#custom-input");
-const resetBtn = document.querySelector("#reset");
+const resetBtn = document.querySelector(".reset");
 const peopleInput = document.querySelector("#number-of-people");
-const amount = document.querySelector(".amount");
-const tipBtns = document.querySelectorAll("tip-btn");
+const amount = document.querySelector(".tip");
+const tipBtns = document.querySelectorAll(".tip-btn");
+const finalBill = document.querySelector(".final-bill");
 let tipValue = 0;
+let tipAmount = 0;
+let billValue = 0;
+let peopleValue = 0;
+
+const blured = () => {
+  if (
+    tipValue === 0 &&
+    tipAmount === 0 &&
+    billValue === 0 &&
+    peopleValue === 0
+    ) {
+    console.log(tipValue, tipAmount, billValue, peopleValue);
+    resetBtn.id = "blured";
+  } else {
+    resetBtn.id = "";
+  }
+};
 
 const sliceInput = (input) => {
   if (input.value.length > 6) {
     input.value = input.value.slice(0, 6);
   }
+  calcBill();
+  calcTip();
+  blured();
 };
 
 const displayError = (input) => {
@@ -26,17 +47,39 @@ tipBtns.forEach((btn) => {
   btn.addEventListener("click", function () {
     tipValue = parseFloat(this.value);
     calcTip();
+    calcBill();
+    blured();
   });
 });
 
+customInput.addEventListener("input", () => {
+  tipValue = parseFloat(customInput.value) / 100;
+  calcBill();
+  calcTip();
+  blured();
+});
+
 const calcTip = () => {
-  let billValue = parseFloat(billInput.value);
-  let peopleValue = parseInt(peopleInput.value);
-  let tipAmount = (billValue / peopleValue) * tipValue;
-  if (Number.isInteger(tipAmount)) {
+  billValue = parseFloat(billInput.value);
+  peopleValue = parseInt(peopleInput.value);
+  tipAmount = (billValue / peopleValue) * tipValue;
+  if (isNaN(tipAmount)) {
+    amount.textContent = "0";
+  } else if (Number.isInteger(tipAmount)) {
     amount.textContent = tipAmount.toFixed(0);
   } else {
     amount.textContent = tipAmount.toFixed(2);
+  }
+};
+
+const calcBill = () => {
+  let billAmount = billValue / peopleValue + tipAmount;
+  if (isNaN(billAmount)) {
+    finalBill.textContent = "0";
+  } else if (Number.isInteger(billAmount)) {
+    finalBill.textContent = billAmount.toFixed(0);
+  } else {
+    finalBill.textContent = billAmount.toFixed(2);
   }
 };
 
@@ -52,15 +95,24 @@ resetBtn.addEventListener("click", () => {
   peopleInput.classList.remove("error");
   billInput.classList.remove("error");
   billInput.value = "";
+  billValue = 0;
+  peopleValue = 0;
+  tipValue = 0;
+  tipAmount = 0;
   customInput.value = "";
   customInput.placeholder = "Custom";
   peopleInput.value = "";
+  amount.textContent = "0";
+  finalBill.textContent = "0";
+  console.log(tipValue, tipAmount, billValue, peopleValue);
+  blured();
 });
 
 billInput.addEventListener("input", () => {
   sliceInput(billInput);
   calcTip();
-  console.log(calcTip());
+  calcBill();
+  blured();
 });
 
 billInput.addEventListener("blur", () => {
@@ -70,6 +122,8 @@ billInput.addEventListener("blur", () => {
 peopleInput.addEventListener("input", () => {
   sliceInput(peopleInput);
   calcTip();
+  calcBill();
+  blured();
 });
 
 peopleInput.addEventListener("blur", () => {
